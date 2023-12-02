de7bad112eaac33704faf9bd1a81171cd884cf75
Max <max.antonio1703.14@gmail.com>
2023-12-02 15:13:01 -0300

    Inicio da criação do modelo professor e modelos adjacentes

--- dev/django/app/models.py
+++ dev/django/app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class Departamento(models.Model):
+    cod_departamento = models.CharField(max_length=10)
+    nome = models.CharField(max_length=200)
+
 class Pessoa(models.Model):
     """
     Uma pessoa, base para outros modelos.
@@ -14,3 +18,17 @@ class Pessoa(models.Model):
         # Como queremos que NÃO seja criada uma tabela para essa classe, ela
         # vira uma ABC.
         abstract = True
+
+
+class Professor(Pessoa):
+    matriculaProfessor = models.CharField(primary_key=True, max_length=12)
+    cod_departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+
+class Projeto(models.Model):
+    id_projeto = models.CharField(max_length=10)
+
+class Proposta(models.Model):
+    id_proposta = models.charField(10)
+    matriculaProfessor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    id_projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE)
