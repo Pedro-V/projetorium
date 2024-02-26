fe9b824a47629d148315a893b2fc94898a7e1d25
Pedro-V <mingas62@hotmail.com>
2024-02-26 19:21:02 -0300

    Reestruturação de diretório dev

---
+++ dev/app/managers.py
@@ -0,0 +1,42 @@
+# accounts/managers.py
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    #Creates and saves a student User.
+    def create_user_student(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_student', True)
+        return self._create_user(email, password, **extra_fields)
+
+    #Creates and saves a teacher User.
+    def create_user_teacher(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_teacher', True)
+        return self._create_user(email, password, **extra_fields)
+
+    #Creates and saves a SuperUser.
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_admin') is not True:
+            raise ValueError('Superuser must have is_admin=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
