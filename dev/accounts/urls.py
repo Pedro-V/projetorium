0252e47e6d27e9f7f2a0d6118cf6c478ed5538b0
Pedro-V <mingas62@hotmail.com>
2024-03-02 15:46:56 -0300

    Caminho raiz leva à pag de login

--- dev/accounts/urls.py
+++ dev/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 from accounts.views import *
 
 urlpatterns = [
+    path("", RedirectView.as_view(url='login/')),
     path("", include('django.contrib.auth.urls')),
     path("cadastro/aluno", CadastrarAluno.as_view(), name="cadastrar_aluno"),
     path("cadastro/professor", CadastrarProfessor.as_view(), name="cadastrar_professor"),
