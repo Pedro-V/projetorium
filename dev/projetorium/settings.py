fd751644b6864942e36509b9e102fb6c46d2a575
Eduardocesn <eduardocesn@academico.ufs.br>
2024-04-05 00:16:58 -0300

    Reset password implementado

--- dev/projetorium/settings.py
+++ dev/projetorium/settings.py
@@ -34,6 +34,13 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'perfil'
 LOGOUT_REDIRECT_URL = 'login'
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'projetoriumreset@gmail.com'
+EMAIL_HOST_PASSWORD = 'yujrxwechltlljcv'
+
 INSTALLED_APPS = [
     'app',
     'accounts',
