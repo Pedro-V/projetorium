ead6bcf4ed679778557fd1f0935cc3543c8a974e
Pedro-V <mingas62@hotmail.com>
2024-04-06 14:57:47 -0300

    Corrigida setting EMAIL_PORT

--- dev/projetorium/settings.py
+++ dev/projetorium/settings.py
@@ -36,7 +36,7 @@ LOGOUT_REDIRECT_URL = 'login'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_POST = 587
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'projetoriumreset@gmail.com'
 EMAIL_HOST_PASSWORD = 'yujrxwechltlljcv'
